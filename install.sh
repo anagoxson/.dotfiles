f79455d7507869aa0592bf2a3484df5841cedc0e
kazuya-morita <oranges99999@gmail.com>
2021-08-12 16:47:49 +0900

    mod install.sh script

--- install.sh
+++ install.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-if [ -x which "brew" ]; then
-    brew install reattach-to-user-namespace
+if [ -x "brew" ]; then
+  brew install reattach-to-user-namespace
 fi
 
 CUR_DIR=$(cd $(dirname $0) && pwd)
 
-for f in $CUR_DIR/.??*
+FNAMES=(".bashrc" ".bash_profile" ".tmux.conf" ".zshrc")
+
+for f in ${FNAMES[@]}
 do
-    filename=$(basename $f)
-    srcpath="$CUR_DIR/$filename"
-    destpath=~/$filename
-    if [ -e $destpath ]; then
-        echo "$destpath already exists."
-    else
-        ln -s $srcpath $destpath
-        echo "created $destpath -> $srcpath"
-    fi
+  srcpath="$CUR_DIR/$f"
+  destpath="$HOME/$f"
+  if [ -e $destpath ]; then
+    echo "$destpath already exists."
+  else
+    ln -s $srcpath $destpath
+    echo "created $destpath -> $srcpath"
+  fi
 done
+
